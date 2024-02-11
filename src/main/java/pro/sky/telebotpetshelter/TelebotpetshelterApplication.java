2ccc8a42e9864a94b412924e129729eebf026275
JustMonik3r <m.mintous@gmail.com>
2024-02-11 14:25:48 +0300

    init

---
+++ src/main/java/pro/sky/telebotpetshelter/TelebotpetshelterApplication.java
@@ -0,0 +1,13 @@
+package pro.sky.telebotpetshelter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TelebotpetshelterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TelebotpetshelterApplication.class, args);
+	}
+
+}
