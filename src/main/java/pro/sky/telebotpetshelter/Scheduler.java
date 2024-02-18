aa1f77469dcc6796c6ab0290015ed8bd145366b1
JustMonik3r <m.mintous@gmail.com>
2024-02-18 20:23:37 +0300

    добавлена сущность, сервис и репозиторий для владельцев питомцев

--- src/main/java/pro/sky/telebotpetshelter/Scheduler.java
+++ src/main/java/pro/sky/telebotpetshelter/Scheduler.java
@@ -1,4 +1,31 @@
 package pro.sky.telebotpetshelter;
 
+import com.pengrad.telegrambot.TelegramBot;
+import com.pengrad.telegrambot.request.SendMessage;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+import pro.sky.telebotpetshelter.repository.DailyReportRepository;
+
+import java.time.LocalDateTime;
+import java.time.temporal.ChronoUnit;
+
+@Component
 public class Scheduler {
+
+    private final DailyReportRepository notificationTaskRepository;
+    private final TelegramBot telegramBot;
+
+    public Scheduler(DailyReportRepository notificationTaskRepository, TelegramBot telegramBot) {
+        this.notificationTaskRepository = notificationTaskRepository;
+        this.telegramBot = telegramBot;
+    }
+
+    @Scheduled(cron = "0 0/1 * * * *")
+    public void sendNotificationTasks() {
+        notificationTaskRepository.findAllByExecDate(LocalDateTime.now().truncatedTo(ChronoUnit.MINUTES))
+                .forEach(notificationTask -> {
+                    SendMessage message = new SendMessage(notificationTask.getChatId(), String.format("Пора выполнить задачу: %s", notificationTask.getText()));
+                    telegramBot.execute(message);
+                });
+    }
 }
---
+++ src/main/java/pro/sky/telebotpetshelter/service/PetOwnerService.java
@@ -0,0 +1,25 @@
+package pro.sky.telebotpetshelter.service;
+
+import pro.sky.telebotpetshelter.entity.PetOwner;
+
+import java.util.List;
+
+public interface PetOwnerService {
+
+    // создание и сохранение нового хозяина
+    PetOwner createOwner(PetOwner petOwner);
+
+    // поиск хозяина по ID
+    PetOwner getOwnerById(Long id);
+
+
+    // получение списка всех хозяев
+    List<PetOwner> getAllOwners();
+
+    // обновление данных хозяина
+    PetOwner updateOwner(PetOwner petOwner);
+
+    //удаление хозяина по id
+    void deleteOwner(Long id);
+
+}
